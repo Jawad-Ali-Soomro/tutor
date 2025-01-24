e9c6108721ddddad9c4130a5abf0e96851a3905d
Jawad <jawadal829@gmail.com>
2025-01-24 16:43:40 +0500

    update of the day!

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import "../styles/login.scss";
+import { FiEyeOff, FiEye } from "react-icons/fi";
+import { RiTwitterXLine, RiGithubLine, RiGoogleLine } from "react-icons/ri";
 
 const Login = ({ onClose }) => {
+    const [showPassword, setShowPassword] = useState(true)
   return (
     <div className="login-container flex" onClick={onClose}>
       <div className="login-wrapper flex col" onClick={(e) => e.stopPropagation()   }  >
@@ -16,10 +21,30 @@ const Login = ({ onClose }) => {
             <input type="text" placeholder="Email Address" />
           </div>
           <div className="input-wrapper flex">
-            <input type="text" placeholder="Password" />
+            <input type={showPassword ? "text" : "Password"}  placeholder="Password" />
+            <div className="icon flex" onClick={() => setShowPassword(!showPassword)}>
+                {
+                    showPassword ? <FiEye />  : <FiEyeOff />
+                }
+            </div>
+          </div>
+          <div className="text flex">
+            <p>Forgot Password?</p>
           </div>
           <div className="btn-blue flex">
             Login   
+          </div>
+          <div className="text-center flex">
+            <p>OR</p>
+          </div>
+          <div className="icons-reg flex">
+            <div className="icon flex">
+                <RiGoogleLine />
+            </div>
+            <div className="icon flex">
+                <RiGithubLine />
+            </div>
+            <button>Register</button>
           </div>
         </div>
       </div>
