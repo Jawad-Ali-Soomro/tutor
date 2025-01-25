e23c3c08a9641558951a718b6792a2758a217313
Jawad <jawadal829@gmail.com>
2025-01-25 10:43:17 +0500

    craeted server and route for user

--- server/index.js
+++ server/index.js
@@ -4,8 +4,13 @@ const app = express();
 require("dotenv").config({
   path: "./config/.env",
 });
-const port = process.env.PORT
-connectDatabase()
-app.listen(port || 4000 , () => {
-    console.log(`server is working at ${port}`)
-})
+const port = process.env.PORT;
+connectDatabase();
+app.use(express.json());
+const cors = require("cors");
+const userRoute = require("./routes");
+app.use('/api/user', userRoute)
+app.use(cors());
+app.listen(port || 4000, () => {
+  console.log(`server is working at ${port}`);
+});
