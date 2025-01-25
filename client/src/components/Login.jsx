383e3d6e6a75c0b2c768638535c2d5902562bf53
Jawad <jawadal829@gmail.com>
2025-01-25 10:18:09 +0500

    update of the day!

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,18 +3,21 @@
 import { useState } from "react";
 import "../styles/login.scss";
 import { FiEyeOff, FiEye } from "react-icons/fi";
-import { RiGithubLine, RiGoogleLine } from "react-icons/ri";
+import { RiGithubLine, RiGoogleLine, RiTwitterXLine } from "react-icons/ri";
 import { BiPurchaseTagAlt } from "react-icons/bi";
 import { HiOutlineBuildingStorefront } from "react-icons/hi2";
 import { PiStudentLight } from "react-icons/pi";
+import { SlCloudUpload } from "react-icons/sl";
 
 const Login = ({ onClose }) => {
   const [showPassword, setShowPassword] = useState(true);
   const [showRole, setShowRole] = useState(false);
-  
+  const [selectedRole, selectRole] = useState("buy");
+  const [showRegistration, setShowRegistration] = useState(false);
+
   return (
     <div className="login-container flex" onClick={onClose}>
-      {!showRole && (
+      {!showRole && !showRegistration && (
         <div
           className="login-wrapper flex col"
           onClick={(e) => e.stopPropagation()}
@@ -55,13 +58,19 @@ const Login = ({ onClose }) => {
               <div className="icon flex">
                 <RiGithubLine />
               </div>
+              <div className="icon flex">
+                <RiTwitterXLine />
+              </div>
               <button onClick={() => setShowRole(true)}>Register</button>
             </div>
           </div>
         </div>
       )}
-      {
-        showRole && <div className="login-wrapper  flex col" onClick={(e) => e.stopPropagation()}>
+      {showRole && (
+        <div
+          className="login-wrapper  flex col"
+          onClick={(e) => e.stopPropagation()}
+        >
           <div className="img-sect flex">
             <img src="/logo.png" alt="" />
           </div>
@@ -69,24 +78,92 @@ const Login = ({ onClose }) => {
             Let's <span>Choose</span> Your Role.
           </h2>
           <div className="choosing-cards flex">
-            <div className="card flex col">
+            <div
+              className="card flex col"
+              style={{
+                background: selectedRole == "buy" ? "royalblue" : "",
+                color: selectedRole == "buy" ? "white" : "",
+              }}
+              onClick={() => selectRole("buy")}
+            >
               <BiPurchaseTagAlt />
               <h2>Buy.</h2>
             </div>
-            <div className="card flex col">
+            <div
+              className="card flex col"
+              style={{
+                background: selectedRole == "sell" ? "royalblue" : "",
+                color: selectedRole == "sell" ? "white" : "",
+              }}
+              onClick={() => selectRole("sell")}
+            >
               <HiOutlineBuildingStorefront />
               <h2>Sell.</h2>
             </div>
-            <div className="card flex col">
+            <div
+              className="card flex col"
+              style={{
+                background: selectedRole == "bargain" ? "royalblue" : "",
+                color: selectedRole == "bargain" ? "white" : "",
+              }}
+              onClick={() => selectRole("bargain")}
+            >
               <PiStudentLight />
               <h2>Bargain.</h2>
             </div>
           </div>
-          <div className="btn-next flex">
+          <div
+            className="btn-next flex"
+            onClick={() => setShowRegistration(true) + setShowRole(false)}
+          >
             NEXT
           </div>
         </div>
-      }
+      )}
+      {!showRole && showRegistration && (
+        <div className="login-wrapper flex col" onClick={(e) => e.stopPropagation()}>
+          <div className="img-sect flex">
+            <img src="/logo.png" alt="" />
+          </div>
+          <h2>
+            Let's <span>Create</span> An Account
+          </h2>
+          <div className="form flex col">
+            {selectedRole != "sell" && (
+              <div className="input-wrapper flex">
+                <input type="text" placeholder="Username" />
+              </div>
+            )}
+            {selectedRole == "sell" && (
+              <div className="input-wrapper flex">
+                <input type="text" placeholder="Store Name" />
+              </div>
+            )}
+            <div className="input-wrapper flex">
+              <input type="text" placeholder="Email Address" />
+            </div>
+            <div className="input-wrapper flex">
+              <input
+                type={showPassword ? "text" : "Password"}
+                placeholder="Password"
+              />
+              <div
+                className="icon flex"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <FiEye /> : <FiEyeOff />}
+              </div>
+            </div>
+              <div className="upload-sect flex col">
+                <SlCloudUpload />
+                <p>Upload {selectedRole == 'sell' ? "Shop Logo" : "Avatar"}</p>
+              </div>
+
+            <div className="btn-blue flex">Register</div>
+          
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,37 +4,8 @@ import { RiScrollToBottomLine } from "react-icons/ri";
 import Latest from "../components/Latest";
 
 const Home = () => {
-  const bookCategories = [
-    {
-      image:
-        "https://booksvilla.com.pk/cdn/shop/files/Read_A_Book_Motivational_Quote_Social_Graphic_12_1200x.png?v=1659092439",
-      name: "English",
-    },
-    {
-      image:
-        "https://booksvilla.com.pk/cdn/shop/files/Read_A_Book_Motivational_Quote_Social_Graphic_2_a6698cea-1112-43cc-aee3-1a93aaa3bcff_1200x.png?v=1701340633  ",
-      name: "Trading",
-    },
-    {
-      image:
-        "https://booksvilla.com.pk/cdn/shop/files/Read_A_Book_Motivational_Quote_Social_Graphic_6_1200x.png?v=1658996161",
-      name: "Children",
-    },
-    {
-      image:
-        "https://booksvilla.com.pk/cdn/shop/files/Read_A_Book_Motivational_Quote_Social_Graphic_5_3b822a0a-b206-4ed6-b2db-dd7c29e629d4_1200x.png?v=1658996084",
-      name: "Poetry",
-    },
-    {
-      image: "https://booksvilla.com.pk/cdn/shop/files/Read_A_Book_Motivational_Quote_Social_Graphic_14_1200x.png?v=1659093417",
-      name: "Fiction",
-    },
-    {
-      image: "https://booksvilla.com.pk/cdn/shop/files/Read_A_Book_Motivational_Quote_Social_Graphic_b6712a66-a616-4595-ae72-7f9e0b306960_1200x.png?v=1701339199",
-      name: "Business",
-    },
-  ];
 
+  
   const imgUrl = [
     "https://booksvilla.com.pk/cdn/shop/files/17667561_314x.jpg?v=1737337099",
     "https://booksvilla.com.pk/cdn/shop/files/15703770_300x.jpg?v=1737336985",
